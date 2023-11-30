affbabd26af59f3331ae7d7b692554ff94d2b6ba
MearnsAaron <143657217+MearnsAaron@users.noreply.github.com>
2023-11-29 21:04:19 -0700

    Update and rename client.c to clientPC.c

--- classwork/classwork14/clientPC.c
+++ classwork/classwork14/clientPC.c
@@ -33,76 +33,76 @@
    #define IP_ADDRESS  "127.0.0.1"
    #define TELNET_PORT 6789
 
-	// not very good at validating ips
-	// but good enough to differentiate between a probably-ip and a probably-not-ip
-	// turns out I don't need this lol
-	int is_ip(const char* s){
-		if(strlen(s) < 1){
-			return 0;
-		}
-		int num_numerics = 0;		
-		for (int i = 0; i < strlen(s); i++){
-			if (!isdigit(s[i]) && s[i] != '.'){
-				return 0;
-			}
-			if (num_numerics == 0 && !isdigit(s[i])){
-				return 0;
-			}
-			if (isdigit(s[i])){
-				num_numerics++;
-				if (num_numerics > 3){
-					return 0;
-				}					
-			}
-			else{
-				num_numerics = 0;
-			}
-		}
-		return 1;
-	}
+    // not very good at validating ips
+    // but good enough to differentiate between a probably-ip and a probably-not-ip
+    // turns out I don't need this lol
+    int is_ip(const char* s){
+        if(strlen(s) < 1){
+            return 0;
+        }
+        int num_numerics = 0;        
+        for (int i = 0; i < strlen(s); i++){
+            if (!isdigit(s[i]) && s[i] != '.'){
+                return 0;
+            }
+            if (num_numerics == 0 && !isdigit(s[i])){
+                return 0;
+            }
+            if (isdigit(s[i])){
+                num_numerics++;
+                if (num_numerics > 3){
+                    return 0;
+                }                    
+            }
+            else{
+                num_numerics = 0;
+            }
+        }
+        return 1;
+    }
 
-	int isnumeric(const char* s){
-		if(strlen(s) < 1){
-			return 0;
-		}
-		for (int i = 0; i < strlen(s); i++){
-			if (!isdigit(s[i])){
-				return 0;
-			}
-		}
-		return 1;
-	}
+    int isnumeric(const char* s){
+        if(strlen(s) < 1){
+            return 0;
+        }
+        for (int i = 0; i < strlen(s); i++){
+            if (!isdigit(s[i])){
+                return 0;
+            }
+        }
+        return 1;
+    }
 
    int main(int argc, char** argv) {
-	    uint32_t port = TELNET_PORT;
-		char* ip_address = IP_ADDRESS;
-		if (argc == 2){
-			if (!isnumeric(argv[1])){
-				ip_address = argv[1];
-			}
-			else{
-				port = strtoul(argv[1], NULL, 10);
-				if (!port){
-					printf("Invalid port number given!\nQuitting now...\n");
-					return 1;
-				}
-			}
-		}
-		else if (argc > 2){
-			int ip_arg = ((!isnumeric(argv[1])) ? 1 : ((!isnumeric(argv[2])) ? 2 : 0));
-			if (!ip_arg){
-				printf("The program must be called in one of the following formats:\n\tclient.exe <ip> <port>\n\tclient.exe <port> <ip>\nQuitting now...\n");
-				return 2;
-			}
-			int port_arg = ((ip_arg = 1) ? 2 : 1);
-			
-			ip_address = argv[ip_arg];
-			port = strtoul(argv[port_arg], NULL, 10);
-			if (!port){
-				printf("Invalid port number given!\nQuitting now...\n");
-				return 3;
-			}
-		}
+        uint32_t port = TELNET_PORT;
+        char* ip_address = IP_ADDRESS;
+        if (argc == 2){
+            if (!isnumeric(argv[1])){
+                ip_address = argv[1];
+            }
+            else{
+                port = strtoul(argv[1], NULL, 10);
+                if (!port){
+                    printf("Invalid port number given!\nQuitting now...\n");
+                    return 1;
+                }
+            }
+        }
+        else if (argc > 2){
+            int ip_arg = ((!isnumeric(argv[1])) ? 1 : ((!isnumeric(argv[2])) ? 2 : 0));
+            if (!ip_arg){
+                printf("The program must be called in one of the following formats:\n\tclient.exe <ip> <port>\n\tclient.exe <port> <ip>\nQuitting now...\n");
+                return 2;
+            }
+            int port_arg = ((ip_arg = 1) ? 2 : 1);
+            
+            ip_address = argv[ip_arg];
+            port = strtoul(argv[port_arg], NULL, 10);
+            if (!port){
+                printf("Invalid port number given!\nQuitting now...\n");
+                return 3;
+            }
+        }
 
 
        //----------------------
@@ -167,32 +167,32 @@
 
        //----------------------
        // Send an initial buffer
-	   	   
-	   printf("Welcome to the client interface!\nEnter messages to send them to the server.\nType \"000\" to quit.\n");
-	   
-	   while(1){
-		   if (fgets(sendbuf, sizeof(sendbuf), stdin)){
-				sendbuf[strcspn(sendbuf, "\n")] = '\0';
-				if (strlen(sendbuf) != 0){
-					iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf) + 1, 0 );
-					if (iResult == SOCKET_ERROR) {
-						wprintf(L"send failed with error: %d\n", WSAGetLastError());
-						closesocket(ConnectSocket);
-						WSACleanup();
-						return 1;
-					}
+              
+       printf("Welcome to the client interface!\nEnter messages to send them to the server.\nType \"000\" to quit.\n");
+       
+       while(1){
+           if (fgets(sendbuf, sizeof(sendbuf), stdin)){
+                sendbuf[strcspn(sendbuf, "\n")] = '\0';
+                if (strlen(sendbuf) != 0){
+                    iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf) + 1, 0 );
+                    if (iResult == SOCKET_ERROR) {
+                        wprintf(L"send failed with error: %d\n", WSAGetLastError());
+                        closesocket(ConnectSocket);
+                        WSACleanup();
+                        return 1;
+                    }
 
-					printf("Bytes Sent: %d with message %s and length %d\n", iResult, sendbuf, strlen(sendbuf) );
+                    printf("Bytes Sent: %d with message %s and length %d\n", iResult, sendbuf, strlen(sendbuf) );
 
-					Sleep(1000);  
-					
-					if(!strcmp(sendbuf, "000")){
-						break;
-					}
-				}
-		   }
-	   }
-	   
+                    Sleep(1000);  
+                    
+                    if(!strcmp(sendbuf, "000")){
+                        break;
+                    }
+                }
+           }
+       }
+       
 
        // shutdown the connection since no more data will be sent
        iResult = shutdown(ConnectSocket, SD_SEND);
