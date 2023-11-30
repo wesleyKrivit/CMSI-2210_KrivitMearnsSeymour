f8763869e7ae162c59ecf0d62919d4fbff839b38
MearnsAaron <143657217+MearnsAaron@users.noreply.github.com>
2023-11-30 10:54:25 -0700

    fibbo.asm

---
+++ classwork/classwork12/fibbo.asm
@@ -0,0 +1,38 @@
+        global _main
+        extern _printf
+        default rel
+        
+        section .data
+num_nums:    dd    10
+num1:        dd    0
+num2:        dd    1
+numstr:      db    "%d ", 0
+
+_main:
+        push    ebx                ; yep
+        mov     eax,    [num1]    
+        mov     ebx,    [num2]     
+        mov     ecx,    [num_nums]
+fibloop:
+        mov     [num1], eax
+        mov     [num2], ebx
+        mov     [num_nums], ecx
+        push    eax
+        push    numstr
+        call    _printf
+        add     esp, 8
+        
+        mov     eax,    [num1]    
+        mov     ebx,    [num2]     
+        mov     ecx,    [num_nums]
+        
+        add     eax, ebx
+        mov     edx, eax
+        mov     eax, ebx
+        mov     ebx, edx
+        
+        dec     ecx
+        cmp     ecx, 0
+        jg      fibloop
+        pop     ebx
+        ret
