4a4c9082b43ecc2f89c8bb0fbbe19a0eaec79fde
MearnsAaron <143657217+MearnsAaron@users.noreply.github.com>
2023-11-30 10:57:38 -0700

    error handling

--- classwork/classwork12/fibbo.asm
+++ classwork/classwork12/fibbo.asm
@@ -7,12 +7,13 @@ num_nums:    dd    10
 num1:        dd    0
 num2:        dd    1
 numstr:      db    "%d ", 0
+errstr:      db    "Totally bogus iterator value, dude!", 0x0A, 0x00
 
 _main:
         push    ebx                ; yep
-        mov     eax,    [num1]    
-        mov     ebx,    [num2]     
-        mov     ecx,    [num_nums]
+        mov     eax, [num1]    
+        mov     ebx, [num2]     
+        mov     ecx, [num_nums]
 fibloop:
         mov     [num1], eax
         mov     [num2], ebx
@@ -22,9 +23,9 @@ fibloop:
         call    _printf
         add     esp, 8
         
-        mov     eax,    [num1]    
-        mov     ebx,    [num2]     
-        mov     ecx,    [num_nums]
+        mov     eax, [num1]    
+        mov     ebx, [num2]     
+        mov     ecx, [num_nums]
         
         add     eax, ebx
         mov     edx, eax
@@ -34,5 +35,12 @@ fibloop:
         dec     ecx
         cmp     ecx, 0
         jg      fibloop
+        jl      fiberror
+done:
         pop     ebx
         ret
+fiberror:
+        push    errstr
+        call    _printf
+        add     esp, 4
+        jmp     done
